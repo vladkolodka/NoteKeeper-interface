f837ec0d2f323faafc509f2fa978d3254c0bf033
DeadShot <vladkolodka@gmail.com>
2016-10-29 19:22:18 +0300

    NewNoteWindow interface

--- NoteKeeper-interface/View/LoginWindow.xaml.cs
+++ NoteKeeper-interface/View/LoginWindow.xaml.cs
@@ -1,27 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace NoteKeeper_interface.View
-{
+namespace NoteKeeper_interface.View {
     /// <summary>
-    /// Interaction logic for LoginWindow.xaml
+    ///     Interaction logic for LoginWindow.xaml
     /// </summary>
-    public partial class LoginWindow : Window
-    {
-        public LoginWindow()
-        {
+    public partial class LoginWindow : Window {
+        public LoginWindow() {
             InitializeComponent();
+        }
+
+        private void SignUpButton_OnClick(object sender, RoutedEventArgs e) {
+            var registerWindow = new RegisterWindow {Owner = this};
+            registerWindow.ShowDialog();
         }
     }
 }
